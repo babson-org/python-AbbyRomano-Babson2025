6750c13c277600b4521f4673d65673a140024842
student-name <student@example.com>
2025-11-13 15:46:18 -0500

    update

--- labs/lab4/portfolio/str_method.py
+++ labs/lab4/portfolio/str_method.py
@@ -1,7 +1,10 @@
+from portfolio import Portfolio 
 
 def portfolio_str(self):
     """TODO: return a readable summary string. self is a Portfolio
     Example (format is flexible):
         "Bob has 2 positions and $1,234.56"
     """
-    return f"COMPLETE THIS LINE"
+    
+    
+    return f"COMPLETE THIS STRING"
