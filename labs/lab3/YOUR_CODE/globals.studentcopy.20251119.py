828eb16a5067eb40b617d9f9f663d17256c5d28a
student-name <student@example.com>
2025-11-19 09:40:36 -0500

    Sync from class repo (20251119)

---
+++ labs/lab3/YOUR_CODE/globals.studentcopy.20251119.py
@@ -0,0 +1,11 @@
+#define global variables that may be used in multiple files 
+#e.g. symbols for mine board, board height, board width, and number of mines 
+MINE_SYMBOL = "💣"
+EMPTY_SYMBOL = " "
+DEFAULT_SYMBOL = "♦"
+ROWS = None
+COLS = None
+MINES = None
+
+
+
